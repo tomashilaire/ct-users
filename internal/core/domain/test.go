f0eb4011dac06c40ea39e36ac2187d878faeaf41
agustinagropro <agustin@agropro.ag>
2022-04-19 14:32:29 -0300

    Added action field to GetTestResponse

--- internal/core/domain/test.go
+++ internal/core/domain/test.go
@@ -1,10 +1,14 @@
 package domain
 
 type Test struct {
-	Id   string `bson:"_id"`
-	Name string `bson:"name"`
+	Id     string `bson:"_id"`
+	Name   string `bson:"name"`
+	Action string `bson: "action"`
 }
 
-func NewTest(Id string, Name string) Test {
-	return Test{Id: Id, Name: Name}
+func NewTest(Id string, Name string, Action string) Test {
+	return Test{
+		Id:     Id,
+		Name:   Name,
+		Action: Action}
 }
