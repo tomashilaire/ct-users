3b4d74034aad9862b509aace75c804d69b2b5d7f
Tomas Hilaire <tomas@agropro.ag>
2022-04-22 13:32:26 -0300

    added files ports

--- internal/core/ports/repositories.go
+++ internal/core/ports/repositories.go
@@ -1,6 +1,7 @@
 package ports
 
 import (
+	"bytes"
 	"test/internal/core/domain"
 )
 
@@ -10,4 +11,9 @@ type TestRepository interface {
 	ShowAll() (t []*domain.Test, err error)
 	ShowById(id string) (t *domain.Test, err error)
 	Delete(id string) error
+}
+
+type FileRepository interface {
+	Save(f *domain.FileInfo) (*domain.FileInfo, error)
+	Load(p string) (*bytes.Buffer, error)
 }
--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -1,6 +1,7 @@
 package ports
 
 import (
+	"bytes"
 	"test/internal/core/domain"
 )
 
@@ -10,4 +11,9 @@ type TestService interface {
 	Update(id string, name string) (*domain.Test, error)
 	Create(name string, action string) (*domain.Test, error)
 	Delete(id string) error
+}
+
+type FilesService interface {
+	Upload(filePath string, fileType string, fileData bytes.Buffer) (string, error)
+	Download(filePath string, id string, fileType string) (*domain.FileInfo, error)
 }
