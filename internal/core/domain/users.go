1f24769fd4a8f524ce3daa99c7061463c94dcf91
Tomas Hilaire <tomas@agropro.ag>
2022-04-25 16:01:49 -0300

    added user struct to domain

---
+++ internal/core/domain/users.go
@@ -0,0 +1,20 @@
+package domain
+
+import "time"
+
+type User struct {
+	Id       string    `bson:"_id" json:"id"`
+	Name     string    `bson:"name" json:"name"`
+	Email    string    `bson:"email" json:"email"`
+	Type     string    `bson:"type" json:"type"`
+	Password string    `bson:"password" json:"password"`
+	Created  time.Time `bson:"created" json:"created"`
+	Updated  time.Time `bson:"updated" json:"updated"`
+}
+
+func NewUser(id string, name string, email string, userType string, password string, created time.Time) *User {
+	return &User{
+		Id: id, Name: name, Email: email, Type: userType,
+		Password: password, Created: created, Updated: created,
+	}
+}
