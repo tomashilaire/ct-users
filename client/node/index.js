c669a2a8811751c47a127e4bef278d61cb08ddb0
Tomas Hilaire <tomas@agropro.ag>
2022-04-27 11:21:51 -0300

    updated node package

--- client/node/index.js
+++ client/node/index.js
@@ -1,6 +1,5 @@
 var PROTO_PATH = __dirname + '/../../internal/handlers/usersprotohdl/users.proto';
-console.log(PROTO_PATH);
-var parseArgs = require('minimist');
+
 var grpc = require('@grpc/grpc-js');
 var protoLoader = require('@grpc/proto-loader');
 var packageDefinition = protoLoader.loadSync(
@@ -13,57 +12,26 @@ var packageDefinition = protoLoader.loadSync(
     });
 var user_proto = grpc.loadPackageDefinition(packageDefinition).pb;
 
-function main() {
-    var argv = parseArgs(process.argv.slice(2), {
-        string: 'target'
-    });
-    var target;
-    if (argv.target) {
-        target = argv.target;
-    } else {
-        target = 'localhost:9092';
-    }
-    var client = new user_proto.Authentication(target,
-        grpc.credentials.createInsecure());
-
-    client.SignUp({
-        name: "thilaire",
-        email: "tomh6@hotmail.com",
-        password: "7410",
-        confirmPassword: "7410",
-        type: "partner"
-    }, function(err, response) {
-        if (err) {
-            console.log(err.message);
-            return
-        }
-        console.log('response:', response);
-        console.log('User ID:', response.user.id);
-    });
-}
-
 function connect(host, port) {
     return new user_proto.Authentication(host+":"+port,
         grpc.credentials.createInsecure());
 }
 
-function signUp(client) {
+function signUp(client, signUpForm, callback) {
     client.SignUp({
-        name: "thilaire",
-        email: "tomh6@hotmail.com",
-        password: "7410",
-        confirmPassword: "7410",
-        type: "partner"
+        name: signUpForm.name,
+        email: signUpForm.email,
+        password: signUpForm.password,
+        confirmPassword: signUpForm.confirmPassword,
+        type: signUpForm.type
     }, function(err, response) {
         if (err) {
             console.log(err.message);
-            return
+            return callback("error");
         }
-        console.log('response:', response);
-        console.log('User ID:', response.user.id);
-        return response.user.id;
+        return callback(response.user.id);
     });
 }
 
-module.exports = connect
-module.exports = signUp
+exports.signUp = signUp;
+exports.connect = connect;
