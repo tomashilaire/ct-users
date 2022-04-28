783ca8ba37fdaacde68e7302315b22c876548890
Tomas Hilaire <tomas@agropro.ag>
2022-04-28 20:05:31 -0300

    added interceptor for testing

--- client/node/connect.js
+++ client/node/connect.js
@@ -12,7 +12,24 @@ let packageDefinition = protoLoader.loadSync(
     });
 let user_proto = grpc.loadPackageDefinition(packageDefinition).pb;
 
+let interceptor = function(options, nextCall) {
+    return new grpc.InterceptingCall(nextCall(options), {
+        sendMessage: function(message, next) {
+            console.log("intercepted");
+            console.log(message);
+            console.log(options.method_descriptor.name)
+            next(message);
+        }
+    });
+};
+
 module.exports = function connect(host, port) {
     return new user_proto.Authentication(host+":"+port,
-        grpc.credentials.createInsecure());
+        grpc.credentials.createInsecure(),
+        {
+            "grpc.keepalive_time_ms": 10000,
+            "grpc.keepalive_permit_without_calls": 1,
+            interceptors: [interceptor],
+        });
 }
+
